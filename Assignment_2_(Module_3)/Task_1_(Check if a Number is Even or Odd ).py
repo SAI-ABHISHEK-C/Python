e2b6d216029e2da27b89ff5c5c997933fcf07985
SAI-ABHISHEK-C <168118648+SAI-ABHISHEK-C@users.noreply.github.com>
2025-09-25 16:09:43 +0530

    Even Odd and Sum of first 50 Integers
    Even–Odd Checker: 
          Reads an integer from the user (or a function arg) and prints whether it’s even or odd using the modulus operator. Includes simple input validation.
    Sum of First 50 Integers: 
         Computes the sum 1+2+⋯+50 using the formula n(n+1)/2 → 1275; also includes an iterative version for clarity.

---
+++ Assignment_2_(Module_3)/Task_1_(Check if a Number is Even or Odd ).py
@@ -0,0 +1,11 @@
+# Taking number from the user
+num = int(input("Enter a number: "))
+
+# Checking odd or even using conditional statements
+
+if (num % 2) == 0:
+    print(num,"is an even number.")
+else:
+    print(num,"is an odd number.")
+
+print("Thank you!")
