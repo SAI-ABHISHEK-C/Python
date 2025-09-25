373016b292d845607f7c7e7972b265371793a6d3
SAI-ABHISHEK-C <168118648+SAI-ABHISHEK-C@users.noreply.github.com>
2025-09-25 11:26:17 +0530

    ASSIGNMENT 1 (Module 2) : Basic Python Concepts
    - Implemented basic arithmetic operations (addition, subtraction, multiplication, division)
    - Added a personalized greeting script to display customized user messages
    - Organized the files under Assignment_1_(Module_2)

---
+++ Assignment_1_(Module_2)/Task_1_(Perform Basic Mathematical Operations).py
@@ -0,0 +1,15 @@
+# Taking 2 operands from the user
+n1 = int(input("Enter the first number: "))
+n2 = int(input("Enter the second number: "))
+
+# Performing basic mathematical operations with the operands
+add = n1 + n2
+sub = n1 - n2
+mul = n1 * n2
+div = n1 / n2
+
+# Printing the output as expected
+print("Addition: ", add)
+print("Subtraction: ", sub)
+print("Multiplication: ", mul)
+print("Division: ", div)
---
+++ Assignment_1_(Module_2)/Task_2_(Create a Personalized Greeting).py
@@ -0,0 +1,6 @@
+# Taking full name as input from the user
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+# Printing a Personalized greeting
+print("Hello, " + first_name + " " + last_name + "! Welcome to the Python program")
